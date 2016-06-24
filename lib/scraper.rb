5eba4dda654a07a1b1abf08a4780a4b5c300e498
lastromanticx <giladbarkan@live.com>
2016-06-24 14:29:37 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,17 +1,56 @@
-require 'pry'
 require 'nokogiri'
 require 'open-uri'
-class Scraper
-  def scrape_index_page(url)
+require 'uri'
 
-    html = File.read(url)
+class Scraper
+  def self.scrape_index_page(url)
+    # :name index.css('.student-card h4').text
+    # :location index.css('.student-card p').text
+    # :profile_url index.css('.student-card a').attribute('href').value
+
+    html = open(url)
     index = Nokogiri::HTML(html)
- binding.pry   
+    cards = []
+
+    index.css('div.student-card').each do |card|
+      cards << {
+        :name => card.css('h4').text,
+        :location => card.css('p').text,
+        :profile_url => "http://127.0.0.1:4000/" + card.css('a').attribute('href').value
+      }
+    end
+
+    cards
   end
 
-  def scrape_profile_page
+  # social profile.css('div.social-icon-container a')[INDEX].attribute('href').value
+  # profile_quote profile.css('div.profile-quote').text
+  # bio profile.css('div.bio-block p').text
+
+  def self.scrape_profile_page(url)
+    html = open(url)
+    profile = Nokogiri::HTML(html)
+    result = {}
+
+    # get name
+    name = profile.css('h1.profile-name').text.downcase.split(/\s+/)
+
+    # get social links
+    profile.css('div.social-icon-container a').each do |a|
+      uri = URI.parse(a.attribute('href').value)
+      key = /(www.)?[^.]+/.match(uri.hostname).to_s.gsub(/www\./,'')
+      
+      if (key).match(name[1]) then key = :blog end
+
+      result[key.to_sym] = a.attribute('href').value
+    end
+ 
+    # get profile quote
+    result[:profile_quote] = profile.css('div.profile-quote').text
+
+    # get bio
+    result[:bio] = profile.css('div.bio-block p').text
+
+    result
   end
 end
-
-a=Scraper.new
-a.scrape_index_page("http://127.0.0.1:4000i/")
