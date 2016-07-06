22d0e7fe3c7a0bf6e291c2cd74b84a78e4e4bfce
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-07-06 09:45:38 -0400

    Done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,44 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    student_array = []
+    index = Nokogiri::HTML(open(index_url))
+    index.css(".student-card").each do |card|
+      student_array.push({
+        :name => card.css(".card-text-container h4.student-name").text,
+        :location => card.css(".card-text-container p.student-location").text,
+        :profile_url => card.css("a").attribute("href").value
+      })
+    end
+
+    student_array
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+    student_hash = {
+      :twitter => nil,
+      :linkedin => nil,
+      :github => nil,
+      :blog => nil
+    }
+    page = Nokogiri::HTML(open(profile_url))
+    #below takes care of social links
+    page.css(".social-icon-container a").each do |link|
+      student_hash.each do |key, field|
+        if link.attribute("href").value.include?(key.to_s)
+          student_hash[key] = link.attribute("href").value
+        end
+      end
+      if !(student_hash.values.include?(link.attribute("href").value))
+        student_hash[:blog] = link.attribute("href").value 
+      end
+    end
 
+    student_hash[:profile_quote] = page.css(".vitals-container div.profile-quote").text
+    student_hash[:bio] = page.css(".bio-block div.description-holder p").text
+
+    student_hash.delete_if{|k, v| v.nil?}
 end
 
+end
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,42 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each{|hash| Student.new(hash)}
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      case key
+      when :twitter
+        @twitter = value
+      when :linkedin
+        @linkedin = value
+      when :github
+        @github = value
+      when :blog
+        @blog = value
+      when :profile_quote
+        @profile_quote = value
+      when :bio
+        @bio = value
+      when :profile_url
+        @profile_url = value
+      end
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
-
