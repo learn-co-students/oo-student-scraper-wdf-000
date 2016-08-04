78ce42473f14dbc2347456c450328fdc137f5fe7
paige11 <ellis.paige.v@gmail.com>
2016-08-04 18:06:52 -0400

    oops didn't read the readme and missed a requirement, will refactor

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,44 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
     
+    roster = []
+    doc.css(".student-card a").each do |object|
+      hash = {}
+      hash[:name] = object.css("h4.student-name").text
+      hash[:location] = object.css("p.student-location").text
+      hash[:profile_url] = "http://127.0.0.1:4000/" + object.attribute("href").value
+      roster << hash
+    end
+
+    roster
   end
 
   def self.scrape_profile_page(profile_url)
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+
+    hash = {}
     
+    doc.css(".social-icon-container a").each do |object|
+      if object.attribute("href").value.include?("twitter")
+        hash[:twitter] = object.attribute("href").value
+      elsif object.attribute("href").value.include?("linkedin")
+        hash[:linkedin] = object.attribute("href").value
+      elsif object.attribute("href").value.include?("github")
+        hash[:github] = object.attribute("href").value
+      else
+        hash[:blog] = object.attribute("href").value
+      end
+    end
+
+    hash[:profile_quote] = doc.css(".profile-quote").text
+    hash[:bio] = doc.css(".description-holder p").text
+
+    hash
+
   end
 
 end
