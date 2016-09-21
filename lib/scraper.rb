af5051433024fe4a532dd2c5fb9320d91a7694d9
Keo  Bun <keonilrath@gmail.com>
2016-09-21 06:30:06 +0000

    scraped

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,59 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_url = Nokogiri::HTML(open("http://104.236.196.127:7061/fixtures/student-site/"))
+
+    index_url.css(".student-card").map do |info|
+      {
+        :name => info.css("h4").text,
+        :location => info.css("p").text,
+        :profile_url => "http://127.0.0.1:4000/" + info.css("a").attribute("href").value
+      }
+    end
   end
+  #uses nokogiri and open_URI to access page; return value is an array of hashes where each hash represents an individual student
+  #keys for Student hash should be :name, :location, :profile_url
+  #colection of students: doc.css(".student-card")
+  #individual student name: doc.css("h4.student-card").text
+  #location: doc.css("p.student-card").text
+  #profile_url: doc.css("a.student-card").attribute("href").value
+  #doc = Nokogiri::HTML(open("http://104.236.196.127:7061/fixtures/student-site/"))
+  #binding.pry
+
+
 
   def self.scrape_profile_page(profile_url)
-    
+    profile = Nokogiri::HTML(open(profile_url))
+
+    student_attributes = {}
+
+    social_media = profile.css("div.social-icon-container a").map {|social| social.attribute("href").value}
+    social_media.each do |social|
+      if social.include?("twitter")
+        student_attributes[:twitter] = social
+      elsif social.include?("linkedin")
+        student_attributes[:linkedin] = social
+      elsif social.include?("github")
+        student_attributes[:github] = social
+      else
+        student_attributes[:blog] = social
+      end
+    end
+
+    student_attributes[:profile_quote] = profile.css("div.profile-quote").text
+    student_attributes[:bio] = profile.css("div.description-holder p").text
+    student_attributes
+
+  #profile_url changed in RSPEC otherwise connection refused
+  #nokogiri & open_URI; return value is hash with key/value pairs for each student, account for students with no social media
+  #:keys are :twitter, :linkedin, :github, :blog,:profile_quote, :bio
+  #social_media: doc.css("div.social-icon-container a")
+  #social_media_sites: doc.css("div.social-icon-container a").attribute("href").value
+  #profile_quote: doc.css("div.profile-quote").text
+  #bio: doc.css("div.description-holder p").text
+  #doc = Nokogiri::HTML(open("http://104.236.196.127:7061/fixtures/student-site/students/ryan-johnson.html"))
+  #binding.pry
+
+
   end
-
 end
-
